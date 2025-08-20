f1959142270c253f138a5dddb2860d256893e491
Willyp63 <wpirino1@gmail.com>
2025-08-20 17:21:35 -0600

    test

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+console.log("YOOOOOOOO");
+console.log(MONGO_URI);
+console.log(PORT);
 if (!MONGO_URI) {
   throw new Error("MONGO_URI environment variable is not defined");
 }
